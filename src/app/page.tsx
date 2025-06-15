5f09205f8e4c9dc0f50671468e281200b45996da
Halldór Óli Ólafsson <113591130+halldoroli@users.noreply.github.com>
2025-06-15 01:14:02 +0000

    Added Next.js with shadcn template (#1)
    * added next template
    
    * added shadcn
    
    * toggle theme button

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+
+export default function Home() {
+  return (
+    <div className="flex h-full justify-center items-center">
+      <ThemeToggle />
+    </div>
+  );
+}

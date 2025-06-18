4379cbcdac2e6ce6337f5dd7d5d0e42a6f32f2e6
halldoroli <halldoroli1@gmail.com>
2025-06-18 22:47:53 +0000

    added title screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import { ThemeToggle } from "@/components/theme-toggle";
+import { Title } from "@/components/title";
+import { Button } from "@/components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex h-full items-center justify-center">
-      <ThemeToggle />
+    <div className="flex flex-col space-y-8 h-full items-center justify-center">
+      <Title />
+      <div className="flex space-x-2">
+        <Button variant="outline">
+          <Link href="game" className="flex items-center">
+            <ChevronRightIcon />
+            Play
+          </Link>
+        </Button>
+        <ThemeToggle />
+      </div>
     </div>
   );
 }

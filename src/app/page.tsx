b449ebbf18b94e7891594c726e109da22f669caf
Halldór Óli Ólafsson <113591130+halldoroli@users.noreply.github.com>
2025-06-18 22:50:52 +0000

    Added title screen (#5)
    * added title screen
    
    * prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import { ThemeToggle } from "@/components/theme-toggle";
+import { Title } from "@/components/title";
+import { Button } from "@/components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex h-full items-center justify-center">
-      <ThemeToggle />
+    <div className="flex h-full flex-col items-center justify-center space-y-8">
+      <Title />
+      <div className="flex space-x-2">
+        <Button variant="outline">
+          <Link href="game" className="flex items-center">
+            <ChevronRightIcon />
+            Play
+          </Link>
+        </Button>
+        <ThemeToggle />
+      </div>
     </div>
   );
 }
---
+++ src/components/title.tsx
@@ -0,0 +1,9 @@
+export function Title() {
+  return (
+    <div className="flex flex-col space-y-2">
+      <h1 className="text-4xl">initialism</h1>
+      <p className="text-gray-500">ini·tial·ism</p>
+      <p>an abbreviation formed from initial letters</p>
+    </div>
+  );
+}

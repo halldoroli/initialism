f306de88e2b5b56bc272ee2c375096d342e725d0
halldoroli <halldoroli1@gmail.com>
2025-06-23 21:59:46 +0000

    Changed fonts to DM

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,9 +1,9 @@
 export function Title() {
   return (
     <div className="flex flex-col space-y-2">
-      <h1 className="text-4xl">initialism</h1>
-      <p className="text-gray-500">ini·tial·ism</p>
-      <p>an abbreviation formed from initial letters</p>
+      <h1 className="text-4xl font-mono">initialism</h1>
+      <p className="text-gray-500 tracking-wide">ini·tial·ism</p>
+      <p className="font-mono">an abbreviation formed from initial letters</p>
     </div>
   );
 }

431eefbce470795a4be723d860620db17e44da7f
halldoroli <halldoroli1@gmail.com>
2025-10-27 16:55:44 +0000

    switch order of enter and backspace

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -9,7 +9,7 @@ const Keyboard = ({ onKeyPress }: KeyboardProps) => {
   const rows: string[][] = [
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-    ["Backspace", "Z", "X", "C", "V", "B", "N", "M", "Enter"],
+    ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Backspace"],
   ];
 
   return (

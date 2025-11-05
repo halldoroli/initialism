3175506c5d01b0704880a89d2a9e3520ac220338
halldoroli <halldoroli1@gmail.com>
2025-11-05 15:58:10 +0000

    added types for kind and categories

---
+++ src/types/category.ts
@@ -0,0 +1,14 @@
+export type Category =
+  | "Organizations"
+  | "Companies"
+  | "Government"
+  | "Tech"
+  | "Science"
+  | "Education"
+  | "Business"
+  | "Entertainment"
+  | "Slang"
+  | "Sports"
+  | "Geography"
+  | "Units"
+  | "Common";

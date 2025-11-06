a2a3fecab5d641264ee3cab8b86252aede839431
halldoroli <halldoroli1@gmail.com>
2025-11-06 21:26:53 +0000

    switched out emojis with icons

--- src/components/category-badge.tsx
+++ src/components/category-badge.tsx
@@ -2,9 +2,50 @@ import { Category } from "@/types/category";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 import { Badge } from "./ui/badge";
 import {
-  categoryDescriptions,
-  categoryIcons,
-} from "@/lib/categories";
+  Building,
+  Banknote,
+  Laptop,
+  Dna,
+  GraduationCap,
+  BarChart,
+  Drama,
+  MessageCircle,
+  Medal,
+  Globe,
+  Ruler,
+  Star,
+} from "lucide-react";
+import { JSX } from "react";
+
+export const categoryIcons: Record<Category, JSX.Element> = {
+  Organizations: <Banknote />,
+  Companies: <Building />,
+  Tech: <Laptop />,
+  Science: <Dna />,
+  Education: <GraduationCap />,
+  Business: <BarChart />,
+  Entertainment: <Drama />,
+  Slang: <MessageCircle />,
+  Sports: <Medal />,
+  Geography: <Globe />,
+  Units: <Ruler />,
+  Common: <Star />,
+};
+
+export const categoryDescriptions: Record<Category, string> = {
+  Organizations: "Government or non-profit agencies and organizations",
+  Companies: "Business brands or companies",
+  Tech: "Technology, computing, or software terms",
+  Science: "Scientific, medical, or research-related terms",
+  Education: "Academic or educational terms",
+  Business: "Finance, economy, or corporate terms",
+  Entertainment: "Media, TV, music, or gaming",
+  Slang: "Internet slang or informal abbreviations",
+  Sports: "Sports-related terms or organizations",
+  Geography: "Countries, cities, or geographical regions",
+  Units: "Measurements, time, or units",
+  Common: "General abbreviations or phrases",
+};
 
 type CategoryBadgeProps = {
   category: Category;
